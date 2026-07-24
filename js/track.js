4b7a229630b6e3187a9218ce10ad55ba3efc48d6
dualpenbooks <dualpen.books@hotmail.com>
2026-07-24 11:33:52 +0200

    Contatore visite anonimo GoatCounter + Privacy generalizzata

--- js/track.js
+++ js/track.js
@@ -46,6 +46,15 @@
   }
   if (!STRICT) loadGtag();
 
+  // Contatore esatto e anonimo (GoatCounter, senza cookie): conta le visite in automatico.
+  (function loadGoat() {
+    var g = document.createElement("script");
+    g.async = true;
+    g.setAttribute("data-goatcounter", "https://terravegan.goatcounter.com/count");
+    g.src = "https://gc.zgo.at/count.js";
+    document.head.appendChild(g);
+  })();
+
   if (!noticeShown()) showNotice();
 
   function showNotice() {
